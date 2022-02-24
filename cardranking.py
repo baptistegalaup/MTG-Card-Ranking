9b10f30e27c137893675889238c935b8e7d0573e
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-24 09:15:14 +0100

    Update cardranking.py

--- cardranking.py
+++ cardranking.py
@@ -5,6 +5,7 @@
 
 
 import streamlit as st
+import pandas as pd
 
 
 # In[2]:
@@ -12,3 +13,6 @@ import streamlit as st
 
 st.title("Card Ranking Comparison Tool")
 
+overview = pd.read_csv(r"C:\Users\Baptiste\Documents\Projets autour de MTG\Projet card ranking\intro.csv", sep=",")
+
+overview
