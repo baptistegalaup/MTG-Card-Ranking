0136c8b2def1e1bfac776480bf97dc966d4bd09f
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-25 14:07:37 +0100

    Update cardranking.py

--- cardranking.py
+++ cardranking.py
@@ -23,7 +23,7 @@ card1 = st.text_input('Card 1')
 proc1 = gihwr['Name'].str.contains(card1.title())
 proc2 = gihwr[proc1]
 
-st.write(proc2)
+st.write(proc2[['Name', 'GIH WR']])
 
 color = proc2['Color']
 
@@ -36,14 +36,21 @@ card2 = st.text_input('Card 2')
 proc3 = gihwr['Name'].str.contains(card2.title())
 proc4 = gihwr[proc3]
 
-st.write(proc4)
+st.write(proc4[['Name', 'GIH WR']])
 
 card3 = st.text_input('Card 3')
 
 proc5 = gihwr['Name'].str.contains(card3.title())
 proc6 = gihwr[proc5]
 
-st.write(proc6)
+st.write(proc6[['Name', 'GIH WR']])
+
+card4 = st.text_input('Card 4')
+
+proc7 = gihwr['Name'].str.contains(card4.title())
+proc8 = gihwr[proc7]
+
+st.write(proc8[['Name', 'GIH WR']])
 
 
 gitview = gitview.replace({'GIH WR': r'%'}, {'GIH WR' : ''}, regex=True)
