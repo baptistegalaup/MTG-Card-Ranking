0226fd535bbee133423b37b1666901a1b722b1e5
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-26 19:56:14 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -7,6 +7,14 @@
 import streamlit as st
 import pandas as pd
 
-st.title('Card Rankking Comparison Tool V2')
+st.title('Card Ranking Comparison Tool V2')
 
 gitview = pd.read_csv('https://raw.githubusercontent.com/baptistegalaup/MTG-Card-Ranking/main/log2302.csv', sep=",")
+
+card1 = st.text_input('Card 1')
+
+proc1 = gitview['Name'].str.contains(card1.title())
+proc2 = gitview[proc1]
+proc3 = proc2[['Name', 'GIH']]
+
+proc3
