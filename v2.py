3ab2a432b87d62754070034fde48125f63be9f4c
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-26 20:23:55 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -10,51 +10,39 @@ import pandas as pd
 st.title('Card Ranking Comparison Tool V2')
 
 gitview = pd.read_csv('https://raw.githubusercontent.com/baptistegalaup/MTG-Card-Ranking/main/log2302.csv', sep=",")
+gihwr = gitview[['Name', 'GIH WR', 'Color']]
+
 
 card1 = st.text_input('Card 1')
 
-proc1 = gitview['Name'].str.contains(card1.title())
-proc2 = gitview[proc1]
-proc3 = proc2[['Name', 'GIH WR']]
+proc1 = gihwr['Name'].str.contains(card1.title())
+proc2 = gihwr[proc1]
 
-st.write(proc3.head(5))
+st.write(proc2[['Name', 'GIH WR']])
 
+
+for i in color:
+  
+  color = i
+  
+  
 card2 = st.text_input('Card 2')
 
-proc4 = gitview['Name'].str.contains(card1.title())
-proc5 = gitview[proc1]
-proc6 = proc5[['Name', 'GIH WR']]
+proc3 = gihwr['Name'].str.contains(card2.title())
+proc4 = gihwr[proc3]
 
-st.write(proc6.head(5))
+st.write(proc4[['Name', 'GIH WR']])
 
 card3 = st.text_input('Card 3')
 
-proc7 = gitview['Name'].str.contains(card1.title())
-proc8 = gitview[proc1]
-proc9 = proc8[['Name', 'GIH WR']]
+proc5 = gihwr['Name'].str.contains(card3.title())
+proc6 = gihwr[proc5]
 
-st.write(proc9.head(5))
+st.write(proc6[['Name', 'GIH WR']])
 
 card4 = st.text_input('Card 4')
 
-proc10 = gitview['Name'].str.contains(card1.title())
-proc11 = gitview[proc10]
-proc12 = proc11[['Name', 'GIH WR']]
+proc7 = gihwr['Name'].str.contains(card4.title())
+proc8 = gihwr[proc7]
 
-st.write(proc12.head(5))
-
-card5 = st.text_input('Card 5')
-
-proc13 = gitview['Name'].str.contains(card1.title())
-proc14 = gitview[proc13]
-proc15 = proc14[['Name', 'GIH WR']]
-
-st.write(proc15.head(5))
-
-card6 = st.text_input('Card 6')
-
-proc16 = gitview['Name'].str.contains(card1.title())
-proc17 = gitview[proc16]
-proc18 = proc17[['Name', 'GIH WR']]
-
-st.write(proc18.head(5))
+st.write(proc8[['Name', 'GIH WR']])
