d6d3df738a78894c0c4542d4082fadf5021e382f
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-27 09:17:55 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -35,7 +35,7 @@ with col1:
 
   if st.button('Click here to add the previous results to your pool', key=1):
 
-    session_state.pool = session_state.pd.concat([pool, proc2[['Name', 'GIH WR']]])
+    session_state.pool = pd.concat([session_state.pool, proc2[['Name', 'GIH WR']]])
 
 
   #presetting the color matching with other good drops
