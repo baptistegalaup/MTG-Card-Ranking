8353657b2d63539d22bcc86099ad70260a7c5fd8
Baptiste Galaup <78553678+baptistegalaup@users.noreply.github.com>
2022-02-27 15:39:29 +0100

    Update v2.py

--- v2.py
+++ v2.py
@@ -16,7 +16,6 @@ st.title('Card Ranking Comparison Tool V2')
 gitview = pd.read_csv('https://raw.githubusercontent.com/baptistegalaup/MTG-Card-Ranking/main/log2602.csv', sep=",")
 gihwr = gitview[['Name', 'GIH WR', 'Color']]
   
-pool = pd.DataFrame(columns=['Name','GIH WR'])
 
 if 'pool' not in st.session_state:
   
